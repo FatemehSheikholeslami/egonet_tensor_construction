1adc8544fe4a9c00b701188b73f65caff45fbc30
FatemehSheikholeslami <33013218+FatemehSheikholeslami@users.noreply.github.com>
2018-02-28 19:46:57 -0600

    Add files via upload

---
+++ build_egoten.py
@@ -0,0 +1,103 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Feb 28 17:43:35 2018
+
+@author: fateme sheikholeslami
+"""
+try:
+    import numpy as np
+    import scipy as sp
+    import networkx as nx
+    import commands
+    import egoten_utility as GEN
+    import argparse
+    from operator import itemgetter
+    import main_fix
+    
+except ImportError as e:
+    print "Application depends on numpy, scipy, matplotlib, networkx"
+    print "Stack trace:"
+    print e.strerror
+
+
+
+def make_egoten(input_adj,directed_flag,nworker,ordered,outputfile):  
+
+  if not ordered: 
+    print 'asdasd'
+    dict_map = main_fix.fix_adj(input_adj)
+    input_adj = 'ordered_'+input_adj  
+  ###################    Form networkx graph    #########################  
+  if directed_flag:  
+    G=GEN.edgelist2networkxG_directed(input_adj)
+  else:   
+    G=GEN.edgelist2networkxG_undirected(input_adj)
+
+  if directed_flag:
+    map_here=GEN.make_ego_tensor_directed(G,outputfile,nworker)
+  else:
+    map_here=GEN.make_ego_tensor_undirected(G,outputfile,nworker)  
+  return
+
+
+  
+  
+#def fix_adj (input_adj):
+#  cmd = 'python main_fix.py '+ input_adj 
+#  (status,output) =   commands.getstatusoutput(cmd)
+#  if status:
+#    print 'Error in ordering network adjacency'
+#    print output
+#  return 'ordered_'+input_adj    
+
+
+
+def parse_args():
+  
+    parser = argparse.ArgumentParser(description="Run tensor_decomp")
+    parser.add_argument('--input', nargs='?', default= 'facebook_adj.edges', 
+	                    help='Input edgelist file, separated by space, default = facebook_adj.edges')
+    parser.add_argument('--output', nargs='?', default= 'egonet_tensor.txt', 
+	                    help='Output file name, default  = communities.txt')	
+    parser.add_argument('--workers', type=int, default=8,
+	                    help='Number of parallel workers for tensor construction. Default is 8.')
+ 
+    parser.add_argument('--directed', dest='directed', action='store_true',
+	                    help='Graph is (un)directed. Default is undirected.')
+    parser.add_argument('--undirected', dest='directed', action='store_false')
+    parser.set_defaults(directed=False)
+    parser.add_argument('--ordered', dest='ordered' ,action = 'store_true',
+	                    help='set if nodes in the edgelist are ordered (no gap in numbering).')
+    parser.add_argument('--unordered', dest='ordered' ,action = 'store_false',
+	                    help='set if ordering of nodes in the edgelist is required. ORIGINAL ORDERING OF NODES WILL BE LOST.')
+    parser.set_defaults(ordered='Error')
+    return parser.parse_args()
+
+
+
+
+if __name__=='__main__':
+
+  args = parse_args()
+  print '************ Running DC_Egoten ****************'
+  print 'Edgelist input ' + args.input
+  print 'Output file ' + args.output
+  print '# of workers for tensor construction : ' + str(args.workers)
+  if args.directed:
+    print 'Network type: Directed'
+  else:
+    print 'Network type: Undirected'
+  if args.ordered:
+    print 'Node-numbering status : ordered'
+  else:
+    print 'Node-numbering status: unordered'
+  if args.ordered=='Error':
+    print '****Usage error: Must set whether the edgelist is ordered or not (if there is gap in oroginal node numbering). See help.****'
+  else:
+    make_egoten(args.input, args.directed , args.workers,args.ordered,args.output)
+  # make_egoten(input_adj,directed_flag,nworker,verbose,ordered,output):  
+
+
+
+
---
+++ main_fix.py
@@ -0,0 +1,70 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Feb 28 17:43:35 2018
+
+@author: fateme sheikholeslami
+"""
+try:
+  import csv
+
+except ImportError as e:
+    print "Application depends on numpy, scipy, matplotlib, networkx"
+    print "Stack trace:"
+    print e.strerror
+
+def main(): 
+  fix_adj(sys.argv[1])
+
+
+def fix_adj(input_adj):
+
+  content = open(input_adj,'r').read().splitlines()
+  #dict_comm={};
+  #with open(inputfile_mode) as f:
+    #content = f.readlines()
+  #matrix = np.zeros((n,Comm))
+  cnt = 1
+  dict_map =dict()
+  g =  open('node_mapping.txt','w') 
+  g.write('#ORIGINAL #ORDERED')
+  g.write (' %d\n' %cnt)
+  open('ordered_'+input_adj, 'w')
+  with open('ordered_'+input_adj, 'w') as f:
+   
+    # let's creat a dictionary mapping new node numbers with the original
+    for line in content:  
+      
+      l =  line.strip().split()
+      if l[0] is not '#':
+        edge_this_line = list()
+        for n in l:
+          if n in dict_map:
+            edge_this_line.append(dict_map[n])
+          else :
+            dict_map[n]=cnt
+            edge_this_line.append(cnt)
+            g.write(n)
+            g.write (' %d\n' %cnt)
+            cnt=cnt+1
+            
+
+
+        f.write('%d ' % edge_this_line[0])
+        f.write('%d \n' % edge_this_line[1])
+      #f.write(('%d %d \n' % edge_this_line[0],edge_this_line[1] ))
+   
+
+  f.close()
+  g.close()
+
+  return dict_map      
+
+#if __name__=='__main__':
+#
+#  import sys
+#
+#       
+#  main()
+
+
